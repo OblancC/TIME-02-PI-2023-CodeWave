a8c8e159dd22d2dbd39807ca0c07a44e65ed1d54
Ary de Souza Oliveira Neto <arineto10@hotmail.com>
2023-06-19 18:19:19 -0300

    fix

--- script.js
+++ script.js
@@ -1,7 +1,92 @@
-function changeColor(element) {
-    element.style.color = "blue";
-}
-
-function resetColor(element) {
-    element.style.color = "";
+{
+    "data" [
+        {
+            "ID Item": "001",
+            "Nome Item": "Produto A",
+            "Tipo": "Tipo A",
+            "Quantidade": 10,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-19"
+        },
+        {
+            "ID Item": "002",
+            "Nome Item": "Produto B",
+            "Tipo": "Tipo B",
+            "Quantidade": 5,
+            "Estado": "Esgotado",
+            "Data Cadastrado": "2023-06-18"
+        },
+        {
+            "ID Item": "003",
+            "Nome Item": "Produto C",
+            "Tipo": "Tipo A",
+            "Quantidade": 8,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-17"
+        },
+        {
+            "ID Item": "004",
+            "Nome Item": "Produto D",
+            "Tipo": "Tipo C",
+            "Quantidade": 2,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-16"
+        },
+        {
+            "ID Item": "005",
+            "Nome Item": "Produto E",
+            "Tipo": "Tipo B",
+            "Quantidade": 0,
+            "Estado": "Esgotado",
+            "Data Cadastrado": "2023-06-15"
+        },
+        {
+            "ID Item": "006",
+            "Nome Item": "Produto F",
+            "Tipo": "Tipo A",
+            "Quantidade": 15,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-14"
+        },
+        {
+            "ID Item": "007",
+            "Nome Item": "Produto G",
+            "Tipo": "Tipo C",
+            "Quantidade": 7,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-13"
+        },
+        {
+            "ID Item": "008",
+            "Nome Item": "Produto H",
+            "Tipo": "Tipo B",
+            "Quantidade": 3,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-12"
+        },
+        {
+            "ID Item": "009",
+            "Nome Item": "Produto I",
+            "Tipo": "Tipo A",
+            "Quantidade": 12,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-11"
+        },
+        {
+            "ID Item": "010",
+            "Nome Item": "Produto J",
+            "Tipo": "Tipo C",
+            "Quantidade": 1,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-10"
+        },
+        {
+            "ID Item": "011",
+            "Nome Item": "Produto K",
+            "Tipo": "Tipo B",
+            "Quantidade": 6,
+            "Estado": "Em estoque",
+            "Data Cadastrado": "2023-06-09"
+        }
+    ]
 }
